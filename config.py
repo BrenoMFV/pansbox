f05069f28c22e58383d11b045d752625d61bccd2
BrenoMFV <brenomfv@gmail.com>
2021-07-25 23:01:13 -0300

    prior connection to database and a few models done. Not tested yet.

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+class Config:
+    DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///:memory:')
+    SQL_ECHO = os.environ.get('SQL_ECHO')
--- models.py
+++ models.py
@@ -1,1 +1,23 @@
-from sqlalchemy import
+import uuid
+
+from . import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.dialects.postgresql import UUID
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column('id', UUID(), default=uuid.uuid4(), primari_key=True, as_uuid=True, index=True)
+    username = Column('username', String(32), unique=True, nullable=False)
+    email = Column('email', String(64))
+    password_hash = Column('password_hash', String(220), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute!")
+
+class Account(Base):
+    __tablename__ = 'accounts'
+
+    id = Column('id', UUID(), default=uuid.uuid4(), primari_key=True, as_uuid=True, index=True)
